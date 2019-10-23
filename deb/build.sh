d07cef8f858f5025caffb6a0bc05eba1464f6dcf
James Fidell <james@openastroproject.org>
2019-10-23 17:51:16 +0100

    Fix duplication of patch files

--- deb/build.sh
+++ deb/build.sh
@@ -33,6 +33,8 @@ case $debversion in
 esac
 echo $compatversion > debfiles/compat
 
+cp ../patches/*.patch debfiles/patches
+
 tar zxf ../libaltaircamlegacy-$version.tar.gz
 cd $srcdir
 test -d demo && ( chmod -x demo/*.* Makefile )
--- deb/clean.sh
+++ deb/clean.sh
@@ -6,3 +6,4 @@ rm -fr libaltaircamlegacy-$version
 rm -fr libaltaircamlegacy_*
 rm -fr libaltaircamlegacy-dev_*
 rm -f debfiles/compat
+rm -f debfiles/patches/*
